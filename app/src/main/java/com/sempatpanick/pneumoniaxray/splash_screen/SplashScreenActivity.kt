c30ddc0e8c7e354d7c04e8dd0e47da68dd68f76d
Muhammad Dadang Saputra <muhdadangs@gmail.com>
2021-05-28 15:47:33 +0700

    Add Splash Screen and Detail History

---
+++ app/src/main/java/com/sempatpanick/pneumoniaxray/splash_screen/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.sempatpanick.pneumoniaxray.splash_screen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sempatpanick.pneumoniaxray.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
