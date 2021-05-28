f9d37423f2d5b2acd5e215cc372f81ade64f964c
Muhammad Dadang Saputra <muhdadangs@gmail.com>
2021-05-28 16:31:55 +0700

    Add Login UI

---
+++ app/src/main/java/com/sempatpanick/pneumoniaxray/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.sempatpanick.pneumoniaxray.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sempatpanick.pneumoniaxray.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
