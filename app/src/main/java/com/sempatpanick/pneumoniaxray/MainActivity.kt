c05eba4aef30e12b3ce82a099ba51ec97a145055
Muhammad Dadang Saputra <muhdadangs@gmail.com>
2021-05-30 11:50:13 +0700

    Add menu logout

--- app/src/main/java/com/sempatpanick/pneumoniaxray/MainActivity.kt
+++ app/src/main/java/com/sempatpanick/pneumoniaxray/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.sempatpanick.pneumoniaxray
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -11,6 +14,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.sempatpanick.pneumoniaxray.core.data.UserRepository
 import com.sempatpanick.pneumoniaxray.core.manager.SessionManager
 import com.sempatpanick.pneumoniaxray.databinding.ActivityMainBinding
+import com.sempatpanick.pneumoniaxray.login.LoginActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -26,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         val sesi = SessionManager(this)
         userRepository = UserRepository.getInstance(sesi)
 
-        Toast.makeText(this,"Welcome ${userRepository.getUser().nama}", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Welcome ${userRepository.getUser().nama}", Toast.LENGTH_SHORT).show()
 
         val navView: BottomNavigationView = binding.navView
 
@@ -39,5 +43,24 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.settings_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_logout) {
+            userRepository.logoutUser()
+            Toast.makeText(this, "Logout success..", Toast.LENGTH_SHORT).show()
+            moveToLogin()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun moveToLogin() {
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
     }
 }
